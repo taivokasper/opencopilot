2732c5150400440dc87136b3764460129d9fce0c
kgrofelnik <kresimir.grofelnik@gmail.com>
2023-08-22 11:42:21 +0200

    Fixes LLM copilot example

--- examples/llm_copilot/app.py
+++ examples/llm_copilot/app.py
@@ -10,12 +10,12 @@ from opencopilot import OpenCopilot
 load_dotenv()
 
 copilot = OpenCopilot(
+    prompt_file="prompts/prompt_template.txt",
     copilot_name="llm",
     auth_type=None,
     helicone_api_key=os.getenv("HELICONE_API_KEY")
 )
 
-copilot.add_prompt("prompts/prompt_template.txt")
 copilot.add_local_files_dir("data")
 
 @copilot.data_loader
